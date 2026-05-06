2bfac0692b356539145db8e7a9a28bc67e503189
alzahara <shahdaljabri33@gmail.com>
2026-05-06 15:22:02 +0400

    Create a Recursive Factorial Program in Java

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -1,23 +1,26 @@
 
 class Factorial {
 
+
     int factorial(int n) {
 
         if (n == 1) {
             return 1;
         }
+
         return n * factorial(n - 1);
     }
-    public class FactorialDemo {
-        public static void main(String[] args) {
-
-            Factorial factObj = new Factorial();
-
-
-            System.out.println("Factorial of 1: " + factObj.factorial(1));
-            System.out.println("Factorial of 2: " + factObj.factorial(2));
-            System.out.println("Factorial of 3: " + factObj.factorial(3));
-        }
-        }
 }
 
+
+public class FactorialDemo {
+    public static void main(String[] args) {
+
+        Factorial factObj = new Factorial();
+
+
+        System.out.println("Factorial of 1: " + factObj.factorial(1));
+        System.out.println("Factorial of 2: " + factObj.factorial(2));
+        System.out.println("Factorial of 3: " + factObj.factorial(3));
+    }
+}
