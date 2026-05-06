a98c843a5d0c5706b3ddb9856ce328d55a638927
alzahara <shahdaljabri33@gmail.com>
2026-05-06 16:33:44 +0400

    Otherwise, keep the first character and recurse

--- src/PairStar.java
+++ src/PairStar.java
@@ -11,4 +11,10 @@ class PairStar {
 
         char firstChar = str.charAt(0);
         char nextChar = str.charAt(1);
+
+        if (firstChar == nextChar) {
+            return firstChar + "*" + pairStar(str.substring(1));
+        } else {
+
+            return firstChar + pairStar(str.substring(1));
 }
