7a1ae5e39071244834342f574ac1c8024d3354ed
alzahara <shahdaljabri33@gmail.com>
2026-05-06 16:39:50 +0400

    If they form abc or aba, count 1 and recurse

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -6,4 +6,12 @@ public class CountAbc {
         }
 
         String firstThree = str.substring(0, 3);
+
+        if (firstThree.equals("abc") || firstThree.equals("aba")) {
+            return 1 + countAbc(str.substring(1));
+        } else {
+
+            return countAbc(str.substring(1));
+        }
+    }
 }
