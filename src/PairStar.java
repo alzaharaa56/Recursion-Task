7687ae4a70e3c7fb5fc0d547463518f9dab7da15
alzahara <shahdaljabri33@gmail.com>
2026-05-06 16:32:11 +0400

    Base case: if string length is 0 or 1, return the string as it is

--- src/PairStar.java
+++ src/PairStar.java
@@ -8,28 +8,4 @@ class PairStar {
             return str;
         }
 
-
-        char firstChar = str.charAt(0);
-        char nextChar = str.charAt(1);
-
-
-        if (firstChar == nextChar) {
-            return firstChar + "*" + pairStar(str.substring(1));
-        } else {
-
-            return firstChar + pairStar(str.substring(1));
-        }
-    }
-}
-
-public class PairStarDemo {
-    public static void main(String[] args) {
-
-        PairStar ps = new PairStar();
-
-
-        System.out.println("pairStar(\"hello\") → " + ps.pairStar("hello"));
-        System.out.println("pairStar(\"xxyy\") → " + ps.pairStar("xxyy"));
-        System.out.println("pairStar(\"aaaa\") → " + ps.pairStar("aaaa"));
-    }
 }
