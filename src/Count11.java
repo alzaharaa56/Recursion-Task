b529dd594e95391c569e070fd001d33a812d89e9
alzahara <shahdaljabri33@gmail.com>
2026-05-07 10:25:10 +0400

    Check if the first two characters are '11'

--- src/Count11.java
+++ src/Count11.java
@@ -2,9 +2,14 @@ public class Count11 {
 
     int count11(String str) {
 
-    }
+
         if (str.length() < 2) {
             return 0;
 
         }
+        if (str.substring(0, 2).equals("11")) {
+
+
+        }
+    }
 }
