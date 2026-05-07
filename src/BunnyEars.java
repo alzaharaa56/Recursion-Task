3f99e9c9ec74765bcae25a0af864bb17a0301162
alzahara <shahdaljabri33@gmail.com>
2026-05-07 08:31:34 +0400

    Test the bunnyEars method with values 0, 1, and 2

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -13,6 +13,9 @@ public class BunnyEarsDemo {
     public static void main(String[] args) {
 
         BunnyEars earsObj = new BunnyEars();
+
+        System.out.println("bunnyEars(0) → " + earsObj.bunnyEars(0));
+        System.out.println("bunnyEars(1) → " + earsObj.bunnyEars(1));
+        System.out.println("bunnyEars(2) → " + earsObj.bunnyEars(2));
     }
 }
-    }
